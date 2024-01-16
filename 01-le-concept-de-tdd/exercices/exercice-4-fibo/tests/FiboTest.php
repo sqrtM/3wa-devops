dff615191ee3c53e09cd1fb340f80863f1010baf
Mason Pike <mpike@flyamelia.com>
2024-01-16 11:50:03 +0100

    fixing some bad answers

--- 01-le-concept-de-tdd/exercices/exercice-4-fibo/tests/FiboTest.php
+++ 01-le-concept-de-tdd/exercices/exercice-4-fibo/tests/FiboTest.php
@@ -1,11 +1,68 @@
 <?php
 
-declare(strict_types=1);
+namespace App\Tests;
 
-class FiboTest extends \PHPUnit\Framework\TestCase
+use App\Fibo;
+use PHPUnit\Framework\TestCase;
+
+class FiboTest extends TestCase
 {
-    public function testSomething()
+    protected Fibo $fibo;
+
+    public function setUp(): void
     {
-        self::assertEquals(1, 1);
+        $this->fibo = new Fibo(max: 10);
+        $this->fibo->run();
+    }
+
+    public function testDefaultMax()
+    {
+        // instance de reflexion class avec le type de classe
+        $reflectionClass = new \ReflectionClass(Fibo::class);
+        // récupérer la pp
+        $reflectionProperty = $reflectionClass->getProperty('max');
+        // on rend accessible la pp
+        $reflectionProperty->setAccessible(true);
+        // on accède à la pp en repassant l'instance de Fibo à getValue
+        $max = $reflectionProperty->getValue(new Fibo()) ;
+
+        $this->assertSame(20, $max);
+    }
+
+    public function testFirstsTermSuite()
+    {
+        // assignation des valeurs nouvelle syntaxe PHP
+        [$num1, $num2] = [$this->fibo->terms[0], $this->fibo->terms[1]];
+
+        $this->assertEquals(1, $num1);
+        $this->assertEquals(1, $num2);
+    }
+
+    /**
+     * @dataProvider terms
+     */
+    public function testConsecutiveTerm($a, $b, $expected, $n)
+    {
+        [$num1, $num2] = [$this->fibo->terms[$n - 1], $this->fibo->terms[$n]];
+
+        $this->assertEquals($num1, $a);
+        $this->assertEquals($num2, $b);
+        $this->assertEquals($expected, $num1 + $num2);
+    }
+
+    public static function terms(): array
+    {
+        return [
+            [1, 1, 2, 1],
+            [1, 2, 3, 2],
+            [2, 3, 5, 3],
+            [3, 5, 8, 4],
+            [5, 8, 13, 5],
+            [8, 13, 21, 6],
+            [13, 21, 34, 7],
+            [21, 34, 55, 8],
+            [34, 55, 89, 9],
+            [55, 89, 144, 10],
+        ];
     }
 }
